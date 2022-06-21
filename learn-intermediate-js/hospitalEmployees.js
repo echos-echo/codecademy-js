72b366d9fe41f36d23a483770ab0bebf50711bb4
Eve Cho <61574136+echos-echo@users.noreply.github.com>
2022-06-20 20:53:17 -0400

    project for classes unit

---
+++ learn-intermediate-js/hospitalEmployees.js
@@ -0,0 +1,48 @@
+// unit: classes
+// objective: practice with classes and inheritance
+
+class HospitalEmployee {
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  
+    static generatePassword() {
+      return Math.floor(Math.random() * 10000);
+    }
+  }
+  
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+    
+    get certifications() {
+      return this._certifications;
+    }
+    
+    addCertification(newCertification) {
+      this.certifications.push(newCertification);
+    }
+  }
+  
+  class Doctor extends HospitalEmployee {
+    constructor(name, insurance) {
+      super(name);
+      this._insurance = insurance;
+    }
+  }
+  
